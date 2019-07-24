b4eb0aa1f78d814ed8dd14209ad4ba7dd47d02a9
zhangqiaoguo <zhangqiaoguo@wuzhenpay.com>
2019-07-24 20:23:19 +0800

    update

--- manager/src/main/java/manager/ManagerApplication.java
+++ manager/src/main/java/manager/ManagerApplication.java
@@ -1,4 +1,4 @@
-package payment;
+package manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- manager/src/main/java/manager/controller/UserController.java
+++ manager/src/main/java/manager/controller/UserController.java
@@ -1,4 +1,4 @@
-package payment.controller;
+package manager.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
